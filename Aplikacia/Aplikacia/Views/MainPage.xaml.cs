c2638adfc601f119ba87ebd1ef26277667ffc344
FilipDavid1 <filipenkodavid@gmail.com>
2021-01-19 13:34:55 +0100

    Refactor BPM program to MVVM design pattern

---
+++ Aplikacia/Aplikacia/Views/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Android.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aplikacia.ViewModels;
+using Xamarin.Forms;
+using static Android.Renderscripts.Element;
+
+namespace Aplikacia.Views
+{
+public partial class MainPage : ContentPage
+{
+    
+
+    public MainPage()
+    {
+        InitializeComponent();
+        
+    }
+}
+}
+
